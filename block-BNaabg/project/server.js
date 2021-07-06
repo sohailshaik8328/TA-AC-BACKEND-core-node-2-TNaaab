6ccf019691afb456bdfbbadcbd60853a5e745f35
sohailshaik8328 <sohailshaik765@gmail.com>
2021-07-06 10:10:00 +0530

    assignment-3

--- block-BNaabg/project/server.js
+++ block-BNaabg/project/server.js
@@ -3,6 +3,7 @@ let http = require("http");
 let path = require("path");
 let url = require("url");
 let qs = require('querystring');
+let userPath = __dirname + "/users/";
 
 
 let server = http.createServer(handleServer);
@@ -14,28 +15,53 @@ function handleServer(req, res) {
     })
 
     req.on("end", () => {
-        // let parsedUrl = JSON.parse(req.url)
-        // console.log(parsedUrl)
+        var parsedUrl = url.parse(req.url, true);
         if(req.url === "/users" && req.method === "POST") {
-            var username = JSON.parse(store).username;
-            fs.open(__dirname+'/users/'+ username + ".json", "wx", (err, fd) => {
+            let username = JSON.parse(store).username;
+            fs.open(userPath + username + ".json", "wx", (err, fd) => {
+                if(err) return console.log(err);
                 fs.writeFile(fd, store, (err) => {
+                    if(err) return console.log(err);
                     fs.close(fd, (err) => {
                         res.end(`${username} successfully created`);
                     })
                 })
             })
         }
-    })
 
-    if(req.url.pathname === "/users" && req.method === "GET") {
-       let parsedUsername =  qs.parse(store).username;
-       console.log(parsedUsername);
-    //    fs.createReadStream( `${req.url}${parsedUsername}`).pipe(res);
-    fs.readFile("./sohailshaik8328.json", (err, user) => {
-        res.end(user)
+        if(parsedUrl.pathname === "/users" && req.method === "GET") {
+            let username = parsedUrl.query.username;
+            fs.readFile(userPath + username + ".json", (err, content) => {
+                if(err) return console.log(err);
+                res.setHeader("Content-Type", "application/json");
+                res.end(content)
+            })            
+        }
+
+        if(parsedUrl.pathname === "/users" && req.method === "DELETE") {
+            let username = parsedUrl.query.username;
+            fs.unlink(userPath + username + ".json", (err) => {
+                if(err) console.log(err);
+                res.end();
+            })
+        }
+
+        if(parsedUrl.pathname === "/users" && req.method === "PUT") {
+            let username = parsedUrl.query.username;
+            fs.open(userPath + username + ".json", "r+", (err, fd) => {
+                if(err) console.log(err);
+                fs.ftruncate(fd, () => {
+                    fs.writeFile(fd, store, (err) => {
+                        if(err) console.log(err);
+                        fs.close(fd, (err) => {
+                            if(err) console.log(err);
+                            res.end(`${username} updated successfully`);
+                        })
+                    })
+                })
+            })
+        }
     })
-    }
 }
 
 
